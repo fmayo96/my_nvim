430ede4b1a2ad492a92a6f3d14d3e8ee4c518b7a
Franco Mayo <francomayo96@gmail.com>
2025-08-04 20:34:46 -0300

    add autocomplete

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.opt.number=true
-vim.opt.relativenumber=true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -13,7 +13,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -29,10 +29,20 @@ vim.cmd.colorscheme("catppuccin")
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<space>fd", builtin.find_files)
-vim.keymap.set("n", "<space>fg", builtin.live_grep) 
-vim.keymap.set("n", "<C-n>", ':Neotree toggle<CR>', { noremap=true, silent=true})
+vim.keymap.set("n", "<space>fg", builtin.live_grep)
+vim.keymap.set("n", "<C-n>", ':Neotree toggle<CR>', { noremap = true, silent = true })
 vim.diagnostic.config({
   virtual_text = {
     prefix = '●', -- Or any other character you prefer
   },
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(args)
+    vim.lsp.buf.format({
+      bufnr = args.buf,
+      timeout_ms = 2000, -- optional
+    })
+  end,
+})
+vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, { desc = "Code Action" })
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,43 @@
 return {
-    {
-      "mason-org/mason.nvim",
-      config = function()
-       require("mason").setup()
-      end
-    },
-    {
-       "mason-org/mason-lspconfig.nvim",
-        config = function()
-          require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls", "ts_ls", "pyright"}
-        })
-        end
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        auto_install = true,
+        ensure_installed = { "ts_ls", "pyright", "gopls", "tailwindcss-language-server", "eslint-lsp" }
+      })
+    end
 
-    },
-    {
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.pyright.setup({})
+      --lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+        end
+      })
+      lspconfig.emmet_ls.setup({
+        filetypes = {
+          'html', 'css', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact'
+        },
+      })
+      lspconfig.pyright.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.eslint.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
       vim.keymap.set("n", "K", vim.lsp.buf.hover)
       vim.keymap.set("n", "gd", vim.lsp.buf.definition)
     end
-    }
+  }
 }
