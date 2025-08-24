289d954e71febe7e369bf4e9bcbce4019570a28b
Franco Mayo <francomayo96@gmail.com>
2025-08-24 12:32:24 -0300

    change clipboard

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -331,19 +331,25 @@ return {
       })
       -- Global diagnostic settings
       vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●",
-          source = "if_many",
-        },
+        virtual_text = false, 
+          --{
+          --prefix = "●",
+          --source = "if_many",
+        --},
         float = {
-          source = "always",
+          border = "rounded",
+          max_width = 80
         },
         signs = true,
         underline = true,
         update_in_insert = false,
         severity_sort = true,
       })
-
+      vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end,
+})
       -- Diagnostic signs
       local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
       for type, icon in pairs(signs) do
